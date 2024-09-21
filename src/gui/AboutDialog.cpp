e0a7a87805ca9b111f52d946093a517af75656a3
HashHound <aravindkishore21@gmail.com>
2024-09-22 00:25:24 +0530

    Update AboutDialog.cpp

--- src/gui/AboutDialog.cpp
+++ src/gui/AboutDialog.cpp
@@ -17,7 +17,7 @@ AboutDialog::AboutDialog(QWidget* _parent) : QDialog(_parent), m_ui(new Ui::Abou
   setWindowTitle(QString(tr("About %1 Wallet")).arg(CurrencyAdapter::instance().getCurrencyDisplayName()));
   QString aboutText = m_ui->m_aboutLabel->text();
   m_ui->m_aboutLabel->setText(aboutText.arg(WALLET_VERSION));
-  m_ui->m_aboutLabel->setText(aboutText.arg(WALLET_VERSION " (" GIT_REVISION "));
+  m_ui->m_aboutLabel->setText(aboutText.arg(WALLET_VERSION " (" GIT_REVISION ")"));
 }
 
 AboutDialog::~AboutDialog() {
