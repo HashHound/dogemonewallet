89e15ae5b62363c641340b66d17e734551e162e7
HashHound <aravindkishore21@gmail.com>
2024-09-22 00:14:51 +0530

    Update Update.cpp

--- src/Update.cpp
+++ src/Update.cpp
@@ -4,12 +4,7 @@
 
 #include "Update.h"
 #include "Settings.h"
-#include <QApplication>
-#include <QDesktopServices>
-#include <QJsonDocument>
-#include <QMessageBox>
-#include <QtNetwork>
-#include <QVersionNumber>
+#include <QDebug>
 
 using namespace WalletGui;
 
@@ -20,43 +15,6 @@ Updater::Updater(QObject *parent) :
 
 void Updater::checkForUpdate()
 {
-  qDebug() << "Check for updates is disabled.";
-}
-
-void Updater::replyFinished (QNetworkReply *reply)
-{
-    if(reply->error())
-    {
-        QString error = QString(tr("Error: %1")).arg(reply->errorString());
-        qDebug() << tr("Unable to check for update") << ": " << error;
-    }
-    else
-    {
-        QString ourVersionStr = Settings::instance().getVersion().split("-")[0];
-        if (ourVersionStr.startsWith(QStringLiteral("v.")))
-            ourVersionStr.remove(0, 2);
-        else if (ourVersionStr.startsWith('v'))
-                 ourVersionStr.remove(0, 1);
-
-        QJsonArray jsonArray = QJsonDocument::fromJson(reply->readAll()).array();
-        QString remoteVersionStr = jsonArray[0].toObject()["name"].toString();
-        if (remoteVersionStr.startsWith(QStringLiteral("v.")))
-            remoteVersionStr.remove(0, 2);
-        else if (remoteVersionStr.startsWith('v'))
-                 remoteVersionStr.remove(0, 1);
-
-        int suffixIndex;
-        QVersionNumber ourVersion = QVersionNumber::fromString(ourVersionStr, &suffixIndex);
-        QVersionNumber remoteVersion = QVersionNumber::fromString(remoteVersionStr, &suffixIndex);
-
-        bool r = QVersionNumber::compare(ourVersion, remoteVersion) < 0;
-        if (r) {
-             if (QMessageBox::warning(nullptr, QObject::tr("New version available"),
-                                      QObject::tr("There is an update available.\nDo you want to go to the download page?"),
-                                      QMessageBox::Ok, QMessageBox::Cancel) == QMessageBox::Ok) {
-                 QDesktopServices::openUrl(QUrl(KARBO_DOWNLOAD_URL));
-             }
-        }
-    }
-    reply->deleteLater();
+    // Dummy implementation, no updates will be checked
+    qDebug() << "Check for updates is disabled.";
 }
